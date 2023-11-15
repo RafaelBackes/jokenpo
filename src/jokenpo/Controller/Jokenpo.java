4e533929643b0e8b5d9fc1a3a0616e82b2828fcc
RafaelBackes <rafaelbackes101@gmail.com>
2023-11-14 21:35:31 -0300

    PRimeiro commit

---
+++ src/jokenpo/Controller/Jokenpo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jokenpo.Controller;
+
+/**
+ *
+ * @author 632120013
+ */
+public class Jokenpo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
