8561b99daacf57eb171c2c7bf32e684e0a42d34f
RafaelBackes <150489826+RafaelBackes@users.noreply.github.com>
2023-11-17 19:52:13 -0300

    Teste 2

--- src/jokenpo/Controller/Jokenpo.java
+++ src/jokenpo/Controller/Jokenpo.java
@@ -16,6 +16,7 @@ public class Jokenpo {
      */
     public static void main(String[] args) {
         System.out.println("Olá Mundo ");
+        System.out.println("Olá Rafael");
         
     }
     
