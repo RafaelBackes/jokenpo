ddc951a0e9b57e32f4bda62959f91d38d003e203
RafaelBackes <150489826+RafaelBackes@users.noreply.github.com>
2023-11-17 19:47:36 -0300

    comit efetuado

--- src/jokenpo/Controller/Jokenpo.java
+++ src/jokenpo/Controller/Jokenpo.java
@@ -15,6 +15,7 @@ public class Jokenpo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Olá Mundo ");
         
     }
     
