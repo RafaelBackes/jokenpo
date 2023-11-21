cc2928222452c824960639abac4381bdb66bebb7
RafaelBackes <rafaelbackes101@gmail.com>
2023-11-21 20:02:18 -0300

    primeiro commit

---
+++ src/jokenpo/Controller/Jokenpo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jokenpo.Controller;
+
+import javax.swing.JFrame;
+import jokenpo.View.TelaPrincipal;
+
+/**
+ *
+ * @author 632120013
+ */
+public class Jokenpo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPrincipal j = new TelaPrincipal();
+        
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setSize(1000,1000);
+        j.setVisible(true);
+        System.out.println("Rafael");
+        
+        
+    }
+    
+}
