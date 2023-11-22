b6b170cf3b6816024c5cdb0644480689a88b4d7e
RafaelBackes <rafaelbackes101@gmail.com>
2023-11-21 21:13:13 -0300

    Primeiro comiit

--- src/jokenpo/Controller/Teste.java
+++ src/jokenpo/Controller/Teste.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package jokenpo.Controller;
 
 /**
  *
  * @author 632120013
  */
-public class novaClasse {
+public class Teste {
     
 }
